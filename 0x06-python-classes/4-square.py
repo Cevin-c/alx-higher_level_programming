fe76b3aeebc200eea24e25ceab90defa95378cef
Cevin-c <cevinbrian13@gmail.com>
2023-10-24 03:40:53 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,48 +1,29 @@
 #!/usr/bin/python3
+""" creates class Square """
 
 
 class Square:
-    """
-    class square that has attributes:
-        size
-    some attributes are protected from input.
-    """
+    """ Square class"""
     def __init__(self, size=0):
-        """
-        initialization function for our square clasee
-        """
-        if self.__validate_size(size):
-            self.__size = size
-
-    @property
-    def size(self):
-        """
-        getter for size attribute
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """
-        setter for size attribute
-        """
-        if self.__validate_size(value):
-            self.__size = value
-
-    def area(self):
-        """
-        calculates the area of the square
-        """
-        return self.__size ** 2
-
-    def __validate_size(self, size):
-        """
-        validates the size, checking for errors
-        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            return True
-        return False
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
